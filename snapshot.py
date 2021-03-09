5a74aa87c77ceafe032f9770107b684dffd5b051
Ryan Heitmann <ryan.heitmann@gmail.com>
2021-03-09 20:58:09 +1300

    Implementing features

--- snapshot.py
+++ snapshot.py
@@ -2,12 +2,24 @@ from picamera import PiCamera, Color
 from time import sleep
 from datetime import datetime
 
-camera = PiCamera()
 
-camera.annotate_text_size = 50
+#Insantiate the camera
+pi_cam = PiCamera()
 
+#Take a photo each minute for 12 hours
+for i in range(720):
 
-#For loop for 24 hours
-#Take a picture every minute
-    #Print the current time
-    
+    #put datetime on screen
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%H:%M")
+
+    #format text
+    pi_cam.annotate_text_size = 50
+    pi_cam.annotate_text = formatted_time
+
+    #sleep 60s
+    sleep(60)
+
+    #camera capture
+    camera.capture('/home/pi/Desktop/snapshotpics/snap_' + formatted_time)
+
